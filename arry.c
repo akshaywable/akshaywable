f54eff371999f1112d292ed12de1bd43b4395f8c
akshaywable <144904948+akshaywable@users.noreply.github.com>
2023-09-13 17:10:02 +0530

    Add files via upload

---
+++ arry.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{ 
+    int i;
+    int arr [10]={0,1,2,3,4,5,6,7,8,9};
+    for ( i=10; i>=1; i--)
+    {
+      printf("%d\n",arr[i]);
+    }
+    
+}
---
+++ arry2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i,sum=0;
+    int arr[5]={0,1,2,3,4};
+    for (i=0;i<5;i++)
+   { sum +=i;
+   }
+    printf("\n the sum is %d",arr[i]);
+   
+}
